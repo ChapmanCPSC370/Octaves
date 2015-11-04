ce64a0a606f71721da72e62d45a2af2b69c3bff8
Levi Johnston <Levi@Levis-MacBook-Pro-2.local>
2015-11-04 00:33:29 -0800

    Created major scale

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -5,55 +5,69 @@ $(function(){
 	var minor = ['w', 'h', 'w', 'w', 'h', 'w', 'w'];
 	$(document).on('click', '.play-scale', function(){
 		var rootNote = $('.scale-choice').find(":selected").val();
-		console.log(rootNote);
+		var endValue = rootNote.slice(-1);
+		var endNote = rootNote.slice(0, - 1); 
+		var temp = parseInt(endValue);
+		temp += 1
+		endNote = endNote + temp.toString();
+
 		var mode = $(".mode");
 		mode = "major";
 		var delay = 0;
+		var before = $( "[data-note='" + rootNote + "']" ).prev();
+		var after = $("[data-note='" + endNote + "']").next()
+
+
 		if(mode == "major"){
-			$(".keyboard-background").children("div").each(function(index, div){
+			$(before).nextUntil(after).each(function(index, div){
 				if(index == 0){
-					delay += 1
 					play(div, delay);
-				}
-				else if(index == 2){
 					delay += 1
 
+				}
+				else if(index == 2){
 					play(div, delay);
+					delay += 1
 				}
 				else if(index == 4){
-					delay += 1
 					play(div, delay);
+					delay += 1
 				}
 				else if(index == 5){
-					delay += 1
 					play(div, delay);
+					delay += 1
 				}
 
 				else if(index == 7){
-					delay += 1
 					play(div, delay);
+					delay += 1
 				}
 				else if(index == 9){
-					delay += 1
 					play(div, delay);
+					delay += 1
 				}
 				else if(index == 11){
-					delay += 1
 					play(div, delay);
+					delay += 1
 				}
 				else if(index == 12){
-					delay += 1
 					play(div, delay);
+					delay += 1
 				}
 			});
 		}
 		else if(mode == "minor"){
 		}
 	});
-	
+
 	function play(div, delay){
 		$(div).delay(delay*700).animate({"background-color": "Red" }, 1000, function(){});
-		$(div).animate({"background-color": "#FFF" }, 200, function(){});
+		if($(div).hasClass("white-key")){
+			$(div).animate({"background-color": "#FFF" }, 200, function(){});
+		}
+		else{
+			$(div).animate({"background-color": "#000" }, 200, function(){});
+		}
 
 	}
 });
