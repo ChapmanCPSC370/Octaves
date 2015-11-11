efdb2d8af73c35889d6ff62b2ef2d69bb61466f0
Levi Johnston <Levi@Levis-MacBook-Pro-2.local>
2015-11-10 23:55:54 -0800

    Made js cleaner. Added more scales. Trying to figure out sound files

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -5,94 +5,70 @@ $(function(){
 	var naturalMinor = ['w', 'h', 'w', 'w', 'h', 'w', 'w'];
 	var harmonicMinor = ['w', 'h', 'w', 'w', 'h', 'w#', 'w'];
 	var melodicMinor = ['w', 'h', 'w', 'w', 'w', 'w', 'h'];
+	var pentatonicBlues = ['w#', 'w', 'h', 'h', 'w#', 'w']
 
 	$(document).on('click', '.play-scale', function(){
 		var rootNote = $('.scale-choice').find(":selected").val();
 		var rootNumber = $("[data-note= '" + rootNote + "']").data("key-number");
 		var mode = $('.mode-choice').find(":selected").val();
 		var delay = 0;
-		var number = rootNumber;
 
 		if(mode == "major"){
-			for(var i = 0; i < major.length; i++){
-				if (number == rootNumber){
-				  play($("[data-key-number= '" + rootNumber + "']"), delay);
-					delay += 1;
-				}
-
-				if(major[i] == "w"){
-					number = number + 2;
-				}
-				else{
-					number += 1;
-				}
-				play($("[data-key-number= '" + number + "']"), delay);
-				delay += 1;
-			}
+			playScale(major, rootNumber, delay);
 		}
 		else if(mode == "natural-minor"){
-			for(var i = 0; i < naturalMinor.length; i++){
-				if (number == rootNumber){
-				  play($("[data-key-number= '" + rootNumber + "']"), delay);
-					delay += 1;
-				}
-				if(naturalMinor[i] == "w"){
-					number = number + 2;
-				}
-				else{
-					number += 1;
-				}
-				play($("[data-key-number= '" + number + "']"), delay);
-				delay += 1;
-			}
+			playScale(naturalMinor, rootNumber, delay);
+
 		}
 		else if(mode == "harmonic-minor"){
-			for(var i = 0; i < harmonicMinor.length; i++){
-				if (number == rootNumber){
-				  play($("[data-key-number= '" + rootNumber + "']"), delay);
-					delay += 1;
-				}
-
-				if(harmonicMinor[i] == "w"){
-					number = number + 2;
-				}
-				else if(harmonicMinor[i] == "h"){
-					number += 1;
-				}
-				else{
-					number += 3;
-				}
-				play($("[data-key-number= '" + number + "']"), delay);
-				delay += 1;
-			}
+			playScale(harmonicMinor, rootNumber, delay);
 		}
 		else if(mode == "melodic-minor"){
-			for(var i = 0; i < melodicMinor.length; i++){
-				if (number == rootNumber){
-				  play($("[data-key-number= '" + rootNumber + "']"), delay);
-					delay += 1;
-				}
+			playScale(melodicMinor, rootNumber, delay);
 
-				if(melodicMinor[i] == "w"){
-					number = number + 2;
-				}
-				else if(melodicMinor[i] == "h"){
-					number += 1;
-				}
-				play($("[data-key-number= '" + number + "']"), delay);
-				delay += 1;
-			}
-		}	
+		}
+		else if(mode == "pentatonic-blues"){
+			playScale(pentatonicBlues, rootNumber, delay);
+		}
 	});
 
+	function playScale(scale, root, delay){
+		var number = root;
+		for(var i = 0; i < scale.length; i++){
+			if (number == root){
+			  play($("[data-key-number= '" + root + "']"), delay);
+				delay += 1;
+			}
+
+			if(scale[i] == "w"){
+				number = number + 2;
+			}
+			else if(scale[i] == "h"){
+				number += 1;
+			}
+			else{
+				number += 3;
+			}
+			play($("[data-key-number= '" + number + "']"), delay);
+			delay += 1;
+		}
+	}
 	function play(div, delay){
-		$(div).delay(delay*700).animate({"background-color": "Red" }, 1000, function(){});
+    //$(".c-note")[0].currentTime = 0
+    //$(".c-note")[0].load();
+    //$(".c-note")[0].play();
+    // var audio = document.createElement("audio");
+    //audio.src = "/assets/c_note.wav"; 
+		//audio.play();
+			
+		$(div).delay(delay*700).animate({"background-color": "Red" }, 600, function(){});
 		if($(div).hasClass("white-key")){
 			$(div).animate({"background-color": "#FFF" }, 200, function(){});
 		}
 		else{
 			$(div).animate({"background-color": "#000" }, 200, function(){});
 		}
+		//$(".c-note")[0].pause();
 
 	}
 });
